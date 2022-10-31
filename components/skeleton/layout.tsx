de652f16f5431684fb8e1464d3133cdc28ab0a1a
Amirsina <amirsina23@gmail.com>
2022-10-31 12:51:18 +0330

    -hotfix(fix_supabase_v2_breaking_changes): update layout component

--- components/skeleton/layout.tsx
+++ components/skeleton/layout.tsx
@@ -1,21 +1,16 @@
-import { AuthSession } from '@supabase/supabase-js'
-import Head from 'next/head'
 import { PropsWithChildren } from 'react'
 import Header from './header'
 import Footer from './footer'
+import Head from 'next/head'
 
-export interface Props {
-  session: AuthSession | null
-}
-
-export function Layout({ session, children }: PropsWithChildren<Props>) {
+export function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <Head>
         <title>Bookabulary</title>
       </Head>
       <div className="flex flex-col min-h-screen bg-gray-50">
-        <Header session={session} />
+        <Header />
         <main className="max-w-screen-xl p-2 m-auto">{children}</main>
         <Footer />
       </div>
