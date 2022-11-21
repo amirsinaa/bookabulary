17a01594b2c344cb9415d54bbdbb5444ac0a1f8a
Amirsina <amirsina23@gmail.com>
2022-11-21 16:13:48 +0330

    -refactor book list fetch query to be compatible with full text search

--- components/book/api/GET_BOOKS_LIST.ts
+++ components/book/api/GET_BOOKS_LIST.ts
@@ -1,7 +1,10 @@
 import type { definitions } from '@/types/supabase-open-api';
 import { supabase } from '@/api/supabase-client';
 
-export const GET_BOOKS_LIST = async () => {
-  const { data, error } = await supabase.from<definitions["books"]>('books').select('*');
+export const GET_BOOKS_LIST = async (title: string) => {
+  const { data, error } = await supabase.from<definitions["books"]>('books').select().textSearch('name', `"${title}"`, {
+    type: 'websearch',
+    config: 'english'
+  });
   return { data, error }
 }
