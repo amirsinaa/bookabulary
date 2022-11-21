5137e7dfc877330e9295732591ffbc99149eb47e
Amirsina <amirsina23@gmail.com>
2022-11-21 16:11:48 +0330

    -feature(ui): add reusable input component

---
+++ components/common/input.tsx
@@ -0,0 +1,3 @@
+export const Input = ({ ...props }) => {
+  return <input className="block w-full py-3 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white" {...props} />;
+}
