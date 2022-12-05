4cf3c4ff1d041a8933fde546cf2006f7f241ed29
Amirsina <amirsina23@gmail.com>
2022-12-05 15:37:11 +0330

    -type safe profile view

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,14 +1,19 @@
-import { ProfileForm } from '@/components/user/views/profile-form';
-import { withPageAuth } from '@supabase/auth-helpers-nextjs'
+import { ProfileForm } from "@/components/user/views/profile-form";
+import { withPageAuth } from "@supabase/auth-helpers-nextjs";
+import { User } from "@/components/user/types/profile";
+import type { GetServerSideProps, NextPage } from "next";
 
-export const getServerSideProps = withPageAuth({
-  redirectTo: '/user/auth',
+
+export const getServerSideProps: GetServerSideProps = withPageAuth({
+  redirectTo: "/user/auth",
   async getServerSideProps(ctx, supabase) {
     const { data: { session: { user } } } = await supabase.auth.getSession();
     return { props: { user } }
   },
 })
 
-export default function Profile({ user }) {
+const ProfilePage: NextPage = ({ user }: User) => {
   return <ProfileForm user={user} />
 }
+
+export default ProfilePage;
