d5e63afdebf18c03495de5d4a293165c6684a608
Amirsina <amirsina23@gmail.com>
2022-12-05 08:38:36 +0330

    -type safe book post query function

--- components/book/api/POST_BOOK.ts
+++ components/book/api/POST_BOOK.ts
@@ -1,8 +1,9 @@
-import type { definitions } from '@/types/supabase-open-api';
-import { supabase } from '@/api/supabase-client';
+import type { definitions } from "@/types/supabase-open-api";
+import type { Book } from "@/components/book/types/book";
+import { supabase } from "@/api/supabase-client";
 
-export const POST_BOOK = async (book) => {
-  const { data, error } = await supabase.from<definitions["books"]>('books').upsert(book).single();
+export const POST_BOOK = async (book: Book) => {
+  const { data, error } = await supabase.from<definitions["books"]>("books").upsert(book).single();
 
   return { data, error }
 }
