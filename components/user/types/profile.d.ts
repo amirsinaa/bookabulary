b445203abd305269ca5737125a440a5bb73d6eb9
Amirsina <amirsina23@gmail.com>
2022-12-05 15:40:28 +0330

    -add new type aliases for user object

--- components/user/types/profile.d.ts
+++ components/user/types/profile.d.ts
@@ -1,11 +1,28 @@
 import { PostgrestError } from '@supabase/supabase-js';
+import { User } from "@supabase/supabase-js";
 import type { Action } from '@/types/react';
+
+export type User = {
+  user: User & {
+    id: string;
+    aud: string;
+    role: string;
+    email: string;
+    email_confirmed_at: string;
+    phone: string;
+    confirmation_sent_at: string;
+    confirmed_at: string;
+    recovery_sent_at: string;
+    last_sign_in_at: string;
+    created_at: string;
+    updated_at: string;
+  }
+}
 
 export interface Profile {
   data: {
-    username?: string,
-    website?: string,
-    avatarUrl?: string
+    username?: string;
+    website?: string;
   };
 }
 
@@ -24,10 +41,4 @@ export type ProfileAction =
   | Action<'SET_LOADING', ProfileLoading>
   | Action<'SET_ERROR', ProfileError>;
 
-
-
 export type ProfileReducerFunc = (state: ProfileState, action: ProfileAction) => ProfileState;
-
-export interface ProfileForm extends Profile {
-  e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>
-}
