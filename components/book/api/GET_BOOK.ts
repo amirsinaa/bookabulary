48ccba25e4b0b3a46cf9af0ea863bc4ca5c65185
Amirsina <amirsina23@gmail.com>
2022-12-05 08:24:15 +0330

    -type safe single book fetch query function

--- components/book/api/GET_BOOK.ts
+++ components/book/api/GET_BOOK.ts
@@ -1,8 +1,8 @@
-import type { definitions } from '@/types/supabase-open-api'
-import { supabase } from '@/api/supabase-client'
+import type { definitions } from "@/types/supabase-open-api";
+import { supabase } from "@/api/supabase-client";
 
-export const GET_BOOK = async (id) => {
-  const { data, error } = await supabase.from<definitions["books"]>('books').select('*').eq('id', id).single();
+export const GET_BOOK = async (id: string) => {
+  const { data, error } = await supabase.from<definitions["books"]>("books").select("*").eq("id", id).single();
 
   return { data, error }
 }
