57f5865e99ab9a9644f1833dbc61e22c126f014c
Amirsina <amirsina23@gmail.com>
2022-12-05 08:37:44 +0330

    -add a new reusable input component

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,3 +1,11 @@
-export const Input = ({ ...props }) => {
-  return <input className="block w-full py-3 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white" {...props} />;
+
+import { LoadingSpinner } from '@/components/common/loading-spinner';
+
+export const Input = ({ isLoading = false, ...props }) => {
+  return (
+    <div className="relative">
+      {isLoading && <LoadingSpinner color="text-teal-800" />}
+      <input className={`block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white }`} {...props} />
+    </div>
+  );
 }
