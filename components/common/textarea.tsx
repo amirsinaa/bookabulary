77ea24237d4f1e2d4a4540b7c4058f2f7d05a51e
Amirsina <amirsina23@gmail.com>
2022-12-05 15:35:35 +0330

    -add a new reusable textarea component

---
+++ components/common/textarea.tsx
@@ -0,0 +1,3 @@
+export const Textarea = ({ ...props }) => {
+  return <textarea className="block w-full pt-10 pb-3 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white" {...props} />;
+}
