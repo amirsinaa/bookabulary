040ae43014efe4870a09af92b6ad724b74d6415e
Amirsina <amirsina23@gmail.com>
2022-12-05 08:25:34 +0330

    -type safe book view

--- pages/books/[id]/index.tsx
+++ pages/books/[id]/index.tsx
@@ -1,14 +1,14 @@
-import { QueryClient, useQuery, dehydrate } from '@tanstack/react-query';
-import { GET_BOOK } from '@/components/book/api/GET_BOOK';
-import { Button } from '@/components/common/button';
-import { Book } from '@/components/book/views/book';
-import { GetServerSideProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { QueryClient, useQuery, dehydrate } from "@tanstack/react-query";
+import { GET_BOOK } from "@/components/book/api/GET_BOOK";
+import type { GetServerSideProps, NextPage } from "next";
+import { Button } from "@/components/common/button";
+import { Book } from "@/components/book/views/book";
+import { useRouter } from "next/router";
 
 const queryClient = new QueryClient();
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { id } = params;
-  await queryClient.prefetchQuery(['book', id], () => GET_BOOK(id));
+  await queryClient.prefetchQuery(["book", id], () => GET_BOOK(String(id)));
 
   return {
     props: {
@@ -17,18 +17,16 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 }
 
-const BooksPage: NextPage = ({ }) => {
+const BooksPage: NextPage = () => {
   const router = useRouter();
   const { query: { id } } = router;
 
-  const { isLoading, data, error } = useQuery(['book', id], () => GET_BOOK(id));
-
-  console.log("is loading" + isLoading)
+  const { isLoading, data, error } = useQuery(["book", id], () => GET_BOOK(String(id)));
   { error && <p>error</p> }
-  { isLoading ? 'Loading ...' : '' }
+  { isLoading ? "Loading ..." : "" }
 
   return (
-    <section className='book-page'>
+    <section className="book-page">
       <Button onClick={() => router.back()}>Go back</Button>
       <Book book={data} />
     </section>
