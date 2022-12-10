8a6b875ba0583ecd8f84d2ac811d7cf173f0c301
Amirsina <amirsina23@gmail.com>
2022-12-10 12:25:34 +0330

    -add vocabulary component

---
+++ components/vocabulary/views/vocabulary.tsx
@@ -0,0 +1,9 @@
+export const Vocabulary = ({ vocabulary, children }) => {
+  const { data: { title, dictonaery } } = vocabulary;
+  return (
+    <article className="flex-row">
+      <h1 className="flex justify-center text-3xl divide-y-2 border-b-3 border-lime-600">{title}</h1>
+      {children}
+    </article>
+  );
+}
