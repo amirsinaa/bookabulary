646f66dfb4bc051db7a55582096b6d5ea198cb0b
Amirsina <amirsina23@gmail.com>
2022-12-17 18:37:28 +0330

    -fix: fix typo

--- components/vocabulary/views/vocabulary.tsx
+++ components/vocabulary/views/vocabulary.tsx
@@ -1,5 +1,5 @@
 export const Vocabulary = ({ vocabulary, children }) => {
-  const { data: { title, dictonaery } } = vocabulary;
+  const { data: { title, dictionary } } = vocabulary;
   return (
     <article className="flex-row">
       <h1 className="flex justify-center text-3xl divide-y-2 border-b-3 border-lime-600">{title}</h1>
