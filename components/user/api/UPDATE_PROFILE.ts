840546662c8c931a96d0c5075e78e3e9a2f4a04b
amirsina <amirsina23@gmail.com>
2022-12-20 15:46:34 +0330

    -bugfix: change profile insert/update method so that insert non-existing user profiles and update existing ones

--- components/user/api/UPDATE_PROFILE.ts
+++ components/user/api/UPDATE_PROFILE.ts
@@ -4,7 +4,7 @@ import { supabase } from '@/api/supabase-client'
 
 export const UPDATE_PROFILE = async (id, updates) => {
 
-  const { error, status } = await supabase.from('profiles').update(updates).eq('id', id).select();
+  const { error, status } = await supabase.from('profiles').upsert(updates).eq('id', id).select();
 
   return {
     error,
