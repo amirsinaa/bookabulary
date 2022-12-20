f2b048e6d8d33192235464214ef658e86301062b
amirsina <amirsina23@gmail.com>
2022-12-20 16:28:48 +0330

    -bugfix: disable book search query on page load

--- components/book/views/book-finder.tsx
+++ components/book/views/book-finder.tsx
@@ -4,20 +4,31 @@ import { BookCard } from "@/components/book/views/book-card";
 import { Button } from '@/components/common/button';
 import { Input } from "@/components/common/input";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import useDebounce from "@/hooks/use-debounce";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export const BookFinder = () => {
-  const [bookName, setBookName] = useState("");
+  const [bookName, setBookName] = useState<string>("");
+  const debounce = useDebounce<string>(bookName, 500);
 
-  const { isLoading, data: books } = useQuery(["book-finder-result", bookName], () => GET_BOOKS_LIST(bookName));
+  const { isLoading, data: books, fetchStatus } = useQuery(
+    ["book-finder-result", debounce],
+    () => GET_BOOKS_LIST(bookName),
+    {
+      enabled: !!bookName
+    }
+  );
   return (
     <section className="flex flex-col flex-wrap justify-center">
       <h3 className="py-10 text-4xl font-bold text-center text-teal-800">Find a book you're reading</h3>
       <div className="relative">
-        {isLoading && <LoadingSpinner color="text-teal-800" />}
-        <Input isLoading={isLoading} type="text" name="bookName" aria-placeholder="Enter a book name" placeholder="Enter a book name" value={bookName} onChange={(e) => setBookName(e.target.value)} />
+        {fetchStatus}
+        {isLoading}
+        {(fetchStatus !== 'idle' && isLoading) && <LoadingSpinner color="text-teal-800" className="absolute right-0 w-10 h-full flex-col justify-center" />}
+        <Input type="text" name="bookName" aria-placeholder="Enter a book name" placeholder="Enter a book name" value={bookName} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBookName(e.target.value)} />
       </div>
+
 
 
       <div className="flex flex-row flex-wrap mb-10 book-search-result">
