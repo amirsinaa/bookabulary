0824c9a4b2e575eb19f4cd4b27e48e01eed9b62f
amirsina <amirsina23@gmail.com>
2022-12-20 16:29:14 +0330

    -code cleanup

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,11 +1,5 @@
-
-import { LoadingSpinner } from '@/components/common/loading-spinner';
-
 export const Input = ({ isLoading = false, ...props }) => {
   return (
-    <div className="relative">
-      {isLoading && <LoadingSpinner color="text-teal-800" />}
-      <input className={`block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white }`} {...props} />
-    </div>
+    <input className={`block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white }`} {...props} />
   );
 }
