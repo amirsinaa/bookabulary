d18dc0a52b5a0dd2610254690dfd40c691dfa2b7
amirsina <amirsina23@gmail.com>
2022-12-26 15:41:46 +0330

    -refactor: extract book search fetching logic into a new cusotm hook

---
+++ components/book/hooks/use-book-search.ts
@@ -0,0 +1,15 @@
+import { GET_BOOKS_LIST } from "@/components/book/api/GET_BOOKS_LIST";
+import { useQuery } from "@tanstack/react-query";
+import useDebounce from "@/hooks/use-debounce";
+
+export const useBookSearch = ({ bookName }) => {
+  const debounce = useDebounce<string>(bookName, 500);
+
+  return useQuery(
+    ["book-finder-result", debounce],
+    () => GET_BOOKS_LIST(bookName),
+    {
+      enabled: !!bookName
+    }
+  );
+}
--- components/book/views/book-finder.tsx
+++ components/book/views/book-finder.tsx
@@ -1,38 +1,25 @@
-import { GET_BOOKS_LIST } from "@/components/book/api/GET_BOOKS_LIST";
+
+import { useBookSearch } from "@/components/book/hooks/use-book-search";
 import { LoadingSpinner } from '@/components/common/loading-spinner';
 import { BookCard } from "@/components/book/views/book-card";
 import { Button } from '@/components/common/button';
 import { Input } from "@/components/common/input";
-import { useQuery } from "@tanstack/react-query";
-import useDebounce from "@/hooks/use-debounce";
 import React, { useState } from "react";
 import Link from "next/link";
 
 export const BookFinder = () => {
   const [bookName, setBookName] = useState<string>("");
-  const debounce = useDebounce<string>(bookName, 500);
+  const books = useBookSearch({ bookName });
 
-  const { isLoading, data: books, fetchStatus } = useQuery(
-    ["book-finder-result", debounce],
-    () => GET_BOOKS_LIST(bookName),
-    {
-      enabled: !!bookName
-    }
-  );
   return (
     <section className="flex flex-col flex-wrap justify-center">
       <h3 className="py-10 text-4xl font-bold text-center text-teal-800">Find a book you're reading</h3>
       <div className="relative">
-        {fetchStatus}
-        {isLoading}
-        {(fetchStatus !== 'idle' && isLoading) && <LoadingSpinner color="text-teal-800" className="absolute right-0 w-10 h-full flex-col justify-center" />}
+        {(books.fetchStatus !== 'idle' && books.isLoading) && <LoadingSpinner color="text-teal-800" className="absolute right-0 w-10 h-full flex-col justify-center" />}
         <Input type="text" name="bookName" aria-placeholder="Enter a book name" placeholder="Enter a book name" value={bookName} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBookName(e.target.value)} />
       </div>
-
-
-
       <div className="flex flex-row flex-wrap mb-10 book-search-result">
-        {books?.data?.map(item => {
+        {books?.data?.data.map(item => {
           return (
             <BookCard key={item.id} book={item} />
           )
