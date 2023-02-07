2014a38a3cd82ef023a6aa29d9a31a7f77a1a18f
amirsina <amirsina23@gmail.com>
2023-02-07 15:58:22 +0330

    -feat(vocabulary): add vocabulary create query function

---
+++ components/vocabulary/api/CREATE_VOCABULARY.ts
@@ -0,0 +1,9 @@
+import type { definitions } from "@/types/supabase-open-api";
+import { supabase } from "@/api/supabase-client";
+
+export const CREATE_VOCABULARY = async (vocabulary) => {
+  const { bookId, updates } = vocabulary
+  const { data, error } = await supabase.from<definitions["vocabulary"]>('vocabulary').upsert(updates).eq('book_id', bookId).single();
+  console.log(data)
+  return { data, error }
+}
