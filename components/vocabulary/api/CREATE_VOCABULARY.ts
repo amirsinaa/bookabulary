b991fbede984e1ee791aacf6ed791e094ca01093
amirsina <amirsina23@gmail.com>
2023-02-07 16:01:52 +0330

    -remove left-over console.log from development

--- components/vocabulary/api/CREATE_VOCABULARY.ts
+++ components/vocabulary/api/CREATE_VOCABULARY.ts
@@ -4,6 +4,6 @@ import { supabase } from "@/api/supabase-client";
 export const CREATE_VOCABULARY = async (vocabulary) => {
   const { bookId, updates } = vocabulary
   const { data, error } = await supabase.from<definitions["vocabulary"]>('vocabulary').upsert(updates).eq('book_id', bookId).single();
-  console.log(data)
+
   return { data, error }
 }
