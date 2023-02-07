4f9a08e67d39b9d3c31e2d0e10fade38b8e02058
amirsina <amirsina23@gmail.com>
2023-02-07 16:02:51 +0330

    -refactor and improve reusable input component

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,5 +1,11 @@
-export const Input = ({ isLoading = false, ...props }) => {
+type InputProps = {
+  isLoading?: boolean,
+  classOverrides?: string | null,
+  [props: string]: unknown
+}
+
+export const Input = ({ isLoading = false, classOverrides = null, ...props }: InputProps) => {
   return (
-    <input className={`block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white }`} {...props} />
+    <input className={`${classOverrides ?? 'block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white'}`}  {...props} />
   );
 }
