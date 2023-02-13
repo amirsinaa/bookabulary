0756f09221ee1cac2c0f7b747b9c6e7efa940f7b
amirsina <amirsina23@gmail.com>
2023-02-13 17:01:52 +0330

    -wrap multipurpose input component in react `forwardRef` to pass down references

--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,11 +1,18 @@
-type InputProps = {
+import { forwardRef } from "react";
+
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
   isLoading?: boolean,
   classOverrides?: string | null,
-  [props: string]: unknown
+  [inputProps: string]: unknown
 }
 
-export const Input = ({ isLoading = false, classOverrides = null, ...props }: InputProps) => {
-  return (
-    <input className={`${classOverrides ?? 'block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:outline-none focus:bg-white'}`}  {...props} />
-  );
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ isLoading = false, classOverrides = null, ...inputProps }, ref) => {
+
+    return (
+      <input className={`${classOverrides ?? 'block w-full py-5 pl-12 pr-4 font-bold text-gray-700 bg-gray-100 rounded-lg shadow focus:bg-white'}`}  {...inputProps} ref={ref} />
+    )
+  }
+);
+
+Input.displayName = 'CustomTailwindInput';
