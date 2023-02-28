92124fc2f65102cb1f1de10e0eed68168308eb16
amirsina <amirsina23@gmail.com>
2023-02-28 11:44:38 +0330

    -add a query function to retrieve list of books owned by a user

--- components/book/hooks/use-users-books-list.ts
+++ components/book/hooks/use-users-books-list.ts
@@ -2,5 +2,5 @@ import { GET_USERS_BOOKS_LIST } from '@/components/book/api/GET_USERS_BOOKS_LIST
 import { useQuery } from '@tanstack/react-query';
 
 export const useUsersBooksList = (userId) => {
-  return useQuery(['books-list', userId], () => GET_USERS_BOOKS_LIST(userId));
+  return useQuery(['users-books-list', userId], () => GET_USERS_BOOKS_LIST(userId));
 }
