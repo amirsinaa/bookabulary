710602e61260cfbb5b41890b17e72221f2df7f87
amirsina <amirsina23@gmail.com>
2023-03-01 17:35:20 +0330

    -improve re-usability of button component

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -1,6 +1,6 @@
 export const Button = ({ children, extraConfig = null, ...props }) => {
   return (
-    <button className={`mx-2 rounded-sm ${!!extraConfig ? extraConfig : ''}`} {...props}>
+    <button className={`mx-2 rounded-md ${!!extraConfig ? extraConfig : ''}`} {...props}>
       {children}
     </button>
   );
