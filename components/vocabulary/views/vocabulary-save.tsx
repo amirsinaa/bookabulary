0b903b8e1105cedb23e0e37c3b817ea238d600b1
amirsina <amirsina23@gmail.com>
2023-03-05 16:17:10 +0330

    -improve code readability and remove left-over console logs

--- components/vocabulary/views/vocabulary-save.tsx
+++ components/vocabulary/views/vocabulary-save.tsx
@@ -3,8 +3,6 @@ import { RocketIcon, MagicWandIcon } from '@radix-ui/react-icons';
 
 const VocabularySave = ({ data, mutation }) => {
   const { vocabularyId } = data
-  console.log(data)
-  console.log(mutation)
   return (
     <button className={`mt-4 rounded-sm w-full btn-light text-xl flex gap-2 py-2 justify-center ${!!mutation.isLoading ? 'animate-pulse' : ''}`} onClick={() => {
       mutation.mutate(vocabularyId === null ? vocabularyMutationMethod('CREATE', data) : vocabularyMutationMethod('UPDATE', data))
