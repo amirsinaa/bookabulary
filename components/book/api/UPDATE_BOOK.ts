b56cba3f96e61a3cf241a9b2d0ea585a13754f14
amirsina <amirsina23@gmail.com>
2023-03-05 16:17:06 +0330

    -improve code readability and remove left-over console logs

--- components/book/api/UPDATE_BOOK.ts
+++ components/book/api/UPDATE_BOOK.ts
@@ -1,8 +1,6 @@
-import type { Book } from "@/components/book/types/book";
 import { supabase } from "@/api/supabase-client";
 
 export const UPDATE_BOOK = async (id, updates) => {
-  console.log(id)
   const { data, error } = await supabase.from('books').upsert(updates).eq('id', id).select();
 
   return { data, error }
