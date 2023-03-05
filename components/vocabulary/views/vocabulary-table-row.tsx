308b75b0f7c61d975accb30683dad42d51e2aa67
amirsina <amirsina23@gmail.com>
2023-03-05 16:18:55 +0330

    -feat(delete_vocabulary_rows): add a functionality to remove vocabulary rows

--- components/vocabulary/views/vocabulary-table-row.tsx
+++ components/vocabulary/views/vocabulary-table-row.tsx
@@ -1,9 +1,11 @@
 import { flexRender } from '@tanstack/react-table';
-const VocabularyTableRow = ({ table }) => {
+import { Cross1Icon } from '@radix-ui/react-icons';
+
+const VocabularyTableRow = ({ table, tableCache, cacheDispatcher, isOwner }) => {
   return (
     table.getRowModel().rows.map(row => {
       return (
-        <tr key={row.id} className='border-b-2 p-0'>
+        <tr key={row.id} className='relative border-b-2 p-0'>
           {row.getVisibleCells().map(cell => {
             return (
               <td key={cell.id} className='border-x-2 p-0'>
@@ -14,10 +16,20 @@ const VocabularyTableRow = ({ table }) => {
               </td>
             )
           })}
+          {isOwner && <span
+            key={`delete-row-${row.id}`}
+            className="hover:cursor-pointer absolute right-2 mt-1"
+            onClick={() => {
+              const dataCopy = [...tableCache];
+              dataCopy.splice(row.id, 1);
+              cacheDispatcher(dataCopy);
+            }}
+          >
+            <Cross1Icon width={34} height={34} type="button" className="text-red-500 bg-transparent hover:text-red-800 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" />
+          </span>}
         </tr>
       )
     })
-
   );
 }
 export default VocabularyTableRow
