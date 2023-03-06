ff27c6b6dcfff610ad9830fb3cf87b3d31311ecc
amirsina <amirsina23@gmail.com>
2023-03-06 07:50:31 +0330

    -feat(private_vocabulary_collection): show a private label tag if the vocabulary is private

--- components/vocabulary/views/vocabulary-card.tsx
+++ components/vocabulary/views/vocabulary-card.tsx
@@ -2,7 +2,7 @@ import { Tag } from "@/components/common/tag"
 import Link from "next/link";
 
 export const VocabularyCard = ({ data, ...props }) => {
-  const { dictionary, title, updated_at } = data;
+  const { dictionary, title, updated_at, is_private } = data;
   const date = new Date(updated_at)?.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 
   return (
@@ -10,8 +10,12 @@ export const VocabularyCard = ({ data, ...props }) => {
       <Link
         key={data?.id}
         href={{ pathname: "/books/vocabulary/[id]", query: { id: data?.id } }}
-        className="hover:cursor-pointer">
+        className="hover:cursor-pointer flex justify-between">
         <h3 className="w-full mx-0 text-2xl font-bold">{title}</h3>
+        {!!is_private && <Tag
+          className="flex items-center px-2 mx-0 text-sm text-white rounded-xl bg-red-600 font-bold">
+          Private
+        </Tag>}
       </Link>
       <span className="text-xs text-slate-500 dark:text-white">Last updated at: {date}</span>
       <p className="mt-2 text-justify vocab-description">This collection contains <Tag>{Object.keys(dictionary?.data)?.length}</Tag> words 🌐</p>
