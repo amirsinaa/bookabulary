0f3cbe51ed638a439f21e3efa26ad21fc42ef285
amirsina <amirsina23@gmail.com>
2023-03-11 17:05:19 +0330

    -feat(feature/homepage_design): add `daisyui` plugin to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
+	daisyui: {
+		themes: [
+			{
+				garden: {
+					primary: '#84cc16',
+					secondary: '#4d7c0f',
+					accent: '#ecfccb',
+					neutral: '#191D24',
+					'base-100': '#2A303C',
+					info: '#115e59',
+					success: '#36D399',
+					warning: '#fb923c',
+					error: '#991b1b',
+				},
+			},
+		],
+	},
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
@@ -8,5 +25,5 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('daisyui')],
 }
