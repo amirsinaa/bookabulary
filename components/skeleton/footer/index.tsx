70f33aadfe88051c099f7e14449249fb9af61cc6
amirsina <amirsina23@gmail.com>
2023-03-12 14:12:21 +0330

    -improve dark mode support

--- components/skeleton/footer/index.tsx
+++ components/skeleton/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer items-center text-neutral-content p-4 bg-white md:px-6 md:py-8 dark:bg-gray-800">
+    <footer className="footer items-center text-neutral-content p-4 bg-white md:px-6 md:py-8 dark:bg-gray-600">
       <div className="items-center grid-flow-col">
         <p>Copyright © {new Date().getFullYear()} Bookabulary</p>
       </div>
