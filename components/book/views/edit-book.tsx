67084116fbc311a4326c432c03dcb8f8fc916949
amirsina <amirsina23@gmail.com>
2023-03-12 14:11:24 +0330

    -replace book actions text with icon

--- components/book/views/edit-book.tsx
+++ components/book/views/edit-book.tsx
@@ -1,11 +1,12 @@
+import { Pencil1Icon } from '@radix-ui/react-icons';
 import { Button } from "@/components/common/button";
 import Link from "next/link";
 
 const EditBook = ({ book }) => {
   return (
-    <Button classOverrides='mx-1 p-2 rounded-lg bg-lime-500 text-white'>
-      <Link href={`/books/${(book.id)}/edit`} className='hover:cursor-pointer px-2'>
-        Edit
+    <Button classOverrides='mx-1 p-2 rounded-lg bg-lime-500 text-white hover:cursor-pointer px-2 dark:bg-lime-400'>
+      <Link className='w-full h-full' href={`/books/${(book.id)}/edit`}>
+        <Pencil1Icon width={20} height={20} />
       </Link>
     </Button>
   );
