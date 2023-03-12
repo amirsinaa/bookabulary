77bfed3680922fa02a752acfd5b5a44e82b9baad
amirsina <amirsina23@gmail.com>
2023-03-12 14:13:47 +0330

    -improve layout ui

--- components/skeleton/layout.tsx
+++ components/skeleton/layout.tsx
@@ -2,12 +2,14 @@ import { useColorMode } from '@/context/color-mode.context';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { Button } from '@/components/common/button';
 import { PropsWithChildren } from 'react';
+import { useRouter } from 'next/router';
 import Header from './header';
 import Footer from './footer';
 import Head from 'next/head';
 
 export function Layout({ children }: PropsWithChildren) {
   const { colorMode, setColorMode } = useColorMode();
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -16,7 +18,7 @@ export function Layout({ children }: PropsWithChildren) {
       <div className={colorMode === 'dark' ? colorMode : 'light'}>
         <div className="bg-white dark:bg-gray-600">
           <Header />
-          <main className="max-w-screen-xl p-2 m-auto h-full min-h-screen relative">
+          <main className={`m-auto h-full min-h-screen relative ${router.pathname === "/" ? '' : 'p-2 max-w-screen-xl'}`}>
             {children}
             <Button onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')} classOverrides='ease-in-out duration-150 hover:drop-shadow-xl fixed bottom-10 left-10 rounded-full border-lime-100 bg-lime-200 drop-shadow-xl p-4 dark:text-black'>{colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}</Button>
           </main>
